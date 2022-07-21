3b7e4e5f3293d59511edf3436d0eb7ff5b8aa9db
Ramadan Wasfi <ramadanwasfi2@gmail.com>
2022-07-21 11:33:53 +0200

    Solve removeFromArray practice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    items = args[0];
+    for(j = 1; j < args.length; j++) {
+        items = removeSingleItemFromArray(items,args[j]);
+    }
+    return items;
+};
 
+const removeSingleItemFromArray = function(items, toBeRemovedItem) {
+    newItems = [];
+
+    for(let i = 0; i < items.length; i++) {
+        if(items[i] === toBeRemovedItem)
+            continue;
+        newItems.push(items[i]);
+    }
+    return newItems;
 };
 
 // Do not edit below this line
