b544d87d5cab69568903e7f6ed303aa3ec435507
Ramadan Wasfi <ramadanwasfi2@gmail.com>
2022-07-21 10:17:14 +0200

    Solve string practice

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (text, numOfRepetiotions) {
+    if (numOfRepetiotions < 0)
+        return 'ERROR';
+    newText = '';
+    for (let i = 0; i < numOfRepetiotions; i++)
+        newText += text;
+    return newText;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (text) {
+    text = text.split('');
+    reversedText = [];
 
+    for (let i = text.length - 1 ; i >= 0; i--) {
+        reversedText.push(text[i]); 
+    }
+    
+    return reversedText.join('');
 };
 
 // Do not edit below this line
