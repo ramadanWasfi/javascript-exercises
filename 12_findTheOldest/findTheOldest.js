5dbf84af57f89a11ee75ea6d3a332202091f725e
Ramadan Wasfi <ramadanwasfi2@gmail.com>
2022-08-05 01:24:48 +0200

    Solve findTheOldest practice

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const result = people.sort((a,b) => {
+        if(a.yearOfDeath === undefined) {
+            a.yearOfDeath = Number((new Date()).getFullYear());
+        }
+        if(b.yearOfDeath === undefined) {
+            b.yearOfDeath = Number((new Date()).getFullYear());
+        }
+        let ageA = a.yearOfDeath - a.yearOfBirth;
+        let ageB = b.yearOfDeath - b.yearOfBirth;
+        return ageA > ageB ? -1 : 1;
+    })
+    return result[0];
 };
 
 // Do not edit below this line
