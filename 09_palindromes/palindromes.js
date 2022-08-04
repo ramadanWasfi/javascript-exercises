164cbc7fdf467a9b813c88df825ba574467079d0
Ramadan Wasfi <ramadanwasfi2@gmail.com>
2022-08-05 00:40:16 +0200

    solve palindrome practice

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    text = removePunctutaion(text);
+    text = text.toLowerCase();
+    palinDrome = true;
+    let start = 0;
+    for (let i = text.length - 1; i >= 0; i--, start++) {
+        if (text[start] !== text[i])
+            palinDrome = false;
 
+    }
+    return palinDrome;
 };
 
+function removePunctutaion(text) {
+    text = text.replace(/!/g, '');
+    text = text.replace('.', '');
+    text = text.replace(/,/g, '');
+    text = text.replace(/ /g, '');
+    return text;
+}
 // Do not edit below this line
 module.exports = palindromes;
